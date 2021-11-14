c9ea5d2641a18f249c5a25869419073f49f11f23
Roberto Paternina <paternina.dev@gmail.com>
2021-11-14 13:58:35 -0500

    viewDetails agregado

--- IMCApp/app/src/main/java/com/example/imcapp/adapters/IMCAdapter.java
+++ IMCApp/app/src/main/java/com/example/imcapp/adapters/IMCAdapter.java
@@ -6,8 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.imcapp.MainActivity;
 import com.example.imcapp.R;
 import com.example.imcapp.db.ImcContract;
 
@@ -26,10 +29,23 @@ public class IMCAdapter extends CursorAdapter {
         TextView txtId = view.findViewById(R.id.txt_id);
         TextView txtDate = view.findViewById(R.id.txt_date);
         TextView txtResult = view.findViewById(R.id.txt_result);
+        ImageButton viewDetails = view.findViewById(R.id.btn_imc_detail);
 
         int _id = cursor.getInt(cursor.getColumnIndexOrThrow(ImcContract.IMCEntry._ID));
         double result = cursor.getDouble(cursor.getColumnIndexOrThrow(ImcContract.IMCEntry.COLUMN_RESULT));
         String strDate = cursor.getString(cursor.getColumnIndexOrThrow(ImcContract.IMCEntry.COLUMN_DATE));
+        double altura = cursor.getDouble(cursor.getColumnIndexOrThrow(ImcContract.IMCEntry.COLUMN_HEIGHT));
+        double peso = cursor.getDouble(cursor.getColumnIndexOrThrow(ImcContract.IMCEntry.COLUMN_WEIGHT));
+
+        viewDetails.setOnClickListener(view1 -> {
+            Toast.makeText(context,
+                    "El dia " + strDate
+                    + " se hizo una medición, con un peso de " + peso
+                    + " y con una altura de " + altura
+                    + " dando como resultado un IMC de: " + result,
+                    Toast.LENGTH_LONG).show();
+
+        });
 
         txtId.setText(String.valueOf(_id));
         txtDate.setText(strDate);
